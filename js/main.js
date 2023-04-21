88cf2b524a764017f449db04839a0bfcecbd79e7
김서현 <ksh9776@naver.com>
2023-04-21 11:35:36 +0900

    러쉬웹수정본

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //jquery
-$(document).ready(function(){
+$(document).ready(function () {
     const section = $('.wrap > section')
     const footer = $('.footer')
 
@@ -21,7 +21,7 @@ $(document).ready(function(){
     const sectionMenu = $('.section-menu')
 
     // resetSection();
-    
+
     // $(window).resize(function(){
     //     wheelCheckFn(); //반복해서 줄이면안보이고 늘이면보이게해줌
     // })
@@ -29,9 +29,9 @@ $(document).ready(function(){
     //각각의섹션에위치파악(y스크롤이동px)
     //화면리사이징이 일어날때마다 호출됨
     // :: 화면리사이징시 변경되는 섹션위치를 다시한번 sectionPos 배열안에 저장함
-    function resetSection(){
-        sectionPos=[];
-        $.each(section,function(index,item){ //요소의각각 wrap의 section들
+    function resetSection() {
+        sectionPos = [];
+        $.each(section, function (index, item) { //요소의각각 wrap의 section들
             let tempY = $(this).offset().top; //각각의 섹션의 위치값(여기선top) 구함
             // console.log(index + ":" + tempY)
 
@@ -42,39 +42,39 @@ $(document).ready(function(){
             console.log(sectionPos);
         })
         //index값이안먹힘(위의 내부변수라서), length:6
-        sectionPos[sectionPos.length]=Math.ceil(footer.offset().top)
+        sectionPos[sectionPos.length] = Math.ceil(footer.offset().top)
     }
     //최초에 새로고침 또는 실행시 위치값파악 -> sectionPos배열에 저장
     resetSection();
 
-    let sectionTotal = sectionPos.length; 
+    let sectionTotal = sectionPos.length;
     // ㄴ> 섹션이몇개인가를저장해놓는곳이total임+footer값도포함 => 7개를 sectiontotal에 집어넣음
     console.log("sectionTotal : " + sectionTotal)
 
     let resizeTimer;
-    $(window).bind('resize',function(){
+    $(window).bind('resize', function () {
         window.clearTimeout(resizeTimer);
-        resizeTimer = window.setTimeout(resizeFunction,500) //setTimeout=timeout시간을set한다는거임
+        resizeTimer = window.setTimeout(resizeFunction, 500) //setTimeout=timeout시간을set한다는거임
     })
 
     //내용물이많이들어 무거워져도 밑내용으로 해주면 에러가 덜발생함.
-    function resizeFunction(){  // 위에서불러온 'resizeFunction'라서 콜백함수여서괄호안함
+    function resizeFunction() {  // 위에서불러온 'resizeFunction'라서 콜백함수여서괄호안함
         //리사이즈시 실행할 코드
         wheelCheckFn();
         resetSection();
-        if(wheeling){
+        if (wheeling) {
             //gsap.to($(요소),durationTime,{설정})
-            gsap.to($('html'),sectionSpeed/1000,{ 
+            gsap.to($('html'), sectionSpeed / 1000, {
                 //gsap:~까지이동시켜라/ 전환타입은 0.5s동안(위에값설정함)
 
-                scrollTop:sectionPos[sectionIndex],
+                scrollTop: sectionPos[sectionIndex],
                 //스크롤탑을 각각섹션위치로 이동시켜라.
 
-                onComplete:function(){  //onComplete이벤트함수, 다완성이되면할일~,
+                onComplete: function () {  //onComplete이벤트함수, 다완성이되면할일~,
                     scrolling = false;  //리사이징될때 위치잡아주려고했던것을 false해라
                 },
             });
-            
+
         }
     }
 
@@ -93,7 +93,7 @@ $(document).ready(function(){
     //                 scrolling = false;  //리사이징될때 위치잡아주려고했던것을 false해라
     //             },
     //         }); 
-            
+
     //     }
     // });
 
@@ -115,13 +115,13 @@ $(document).ready(function(){
         if (scrolling) {  //scrolling의 초기값은 false
             return;
         }
-        scrolling=true; 
+        scrolling = true;
         //★ 마우스휠작동막기=true, flase=마우스휠작동막지않음
 
         if (distance < 0) {  //distance < 0 -> 음수
             sectionIndex++;  //section의고유넘버
             if (sectionIndex >= sectionTotal) {
-                sectionIndex = sectionTotal-1;
+                sectionIndex = sectionTotal - 1;
             }
             console.log(sectionIndex)
 
@@ -132,9 +132,9 @@ $(document).ready(function(){
             }
             console.log(sectionIndex)
         }
-        gsap.to($('html'), sectionSpeed / 1000,{
+        gsap.to($('html'), sectionSpeed / 1000, {
             scrollTo: sectionPos[sectionIndex],
-            onComplete: function(){
+            onComplete: function () {
                 scrolling = false
             }
         });
@@ -143,25 +143,35 @@ $(document).ready(function(){
         sectionColor();
     })
 
+    //bestswiper
+    var swiper111 = new Swiper(".best-swiper", {
+        // direction: 'vertical',
+        autoplay: {
+            delete: 3000,
+            disableOnInteraction: false,
+        },
+        loop: Infinity,
+    });
+
     //section메뉴클릭시 섹션이동
     const sectionLink = $('.section-menu a')
-    $.each(sectionLink, function(index, item){
-        $(this).click(function(e){
+    $.each(sectionLink, function (index, item) {
+        $(this).click(function (e) {
             e.preventDefault();
             moveSection(index);
             sectionColor();
         })
     })
-    function moveSection(_index){
+    function moveSection(_index) {
         sectionIndex = _index;  //sectionIndex전역변수임// 전역변수여서 앞에 let쓰면안됨
-        gsap.to($('html'), sectionSpeed/1000,{
-            scrollTo:sectionPos[sectionIndex], //scroll to ~로가라
-            oncomplete: function(){
-                scrolling=false;
+        gsap.to($('html'), sectionSpeed / 1000, {
+            scrollTo: sectionPos[sectionIndex], //scroll to ~로가라
+            oncomplete: function () {
+                scrolling = false;
             }
         })
     }
-    function sectionColor(){
+    function sectionColor() {
         //포커스표현
         sectionLink.removeClass('section-menu-active') //class라고적혔으면 .안찍어야함
         sectionLink.eq(sectionIndex).addClass('section-menu-active')
@@ -175,34 +185,34 @@ $(document).ready(function(){
 
 
 // vanilla Javascript
-window.onload = function(){
-    
+window.onload = function () {
+
 };
 
 //swiper
 var swiper = new Swiper(".mySwiper", {
-	scrollbar: {
-		el: ".swiper-scrollbar",
-		hide: true,
-	},
-	autoplay: {
-        delete:2000,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    autoplay: {
+        delete: 2000,
         disableOnInteraction: false,
     },
-	loop: Infinity,
+    loop: Infinity,
 });
 
 //footer-swiper
 var swiper = new Swiper(".main04swiper", {
-	scrollbar: {
-		el: ".main04-swiper-scrollbar",
-		hide: true,
-	},
-	autoplay: {
-        delete:2000,
+    scrollbar: {
+        el: ".main04-swiper-scrollbar",
+        hide: true,
+    },
+    autoplay: {
+        delete: 2000,
         disableOnInteraction: false,
     },
-	loop: Infinity,
+    loop: Infinity,
     disableOnInteraction: false,
 });
 
@@ -216,7 +226,7 @@ var swiper = new Swiper(".main04swiper", {
 // 		e.preventDefault();
 // 		tabAnchor.removeClass('active');
 // 		$(this).addClass('active');
-      
+
 // 		var tabIdx = $(this).index();
 //         console.log(tabIdx)
 
@@ -235,70 +245,70 @@ var swiper = new Swiper(".main04swiper", {
 
 
 $(function () {
-    const tabAnchor = $('.main03-text-wrap a'), 
+    const tabAnchor = $('.main03-text-wrap a'),
         tabPanel = $('.tab')
-         tabAnchor.click(function(e){
-            e.preventDefault();
-    
-            tabAnchor.removeClass('active');
-            $(e.currentTarget).addClass("active")
-    
-            tabPanel.hide();
-            let target =$(this).attr('href')
-            $(target).show();
-    
-         })
-         tabAnchor.eq(0).trigger("click")
-    });
-    
+    tabAnchor.click(function (e) {
+        e.preventDefault();
+
+        tabAnchor.removeClass('active');
+        $(e.currentTarget).addClass("active")
+
+        tabPanel.hide();
+        let target = $(this).attr('href')
+        $(target).show();
+
+    })
+    tabAnchor.eq(0).trigger("click")
+});
+
 //click-on
-    // $(function () {
-    //     const tabAnchor = $('.main3-touch-tab ul > li a'),
-    //         tabPanel = $('.main-event3-list')
-    //         //링크를 클릭했을때 할 일
-    //          tabAnchor.click(function(e){
-    //             e.preventDefault();
-        
-    //             tabAnchor.removeClass('on'); //전체를 클리어해놓고
-    //             $(e.currentTarget).addClass("on")//내가 선택한거 active된다 this로 적어줘도된다 
-        
-    //             tabPanel.hide(); //display:none;
-    //             let target =$(this).attr('href')
-    //             $(target).show();
-        
-    //          })
-    //          tabAnchor.eq(0).trigger("click")
-    //     });
+// $(function () {
+//     const tabAnchor = $('.main3-touch-tab ul > li a'),
+//         tabPanel = $('.main-event3-list')
+//         //링크를 클릭했을때 할 일
+//          tabAnchor.click(function(e){
+//             e.preventDefault();
+
+//             tabAnchor.removeClass('on'); //전체를 클리어해놓고
+//             $(e.currentTarget).addClass("on")//내가 선택한거 active된다 this로 적어줘도된다 
+
+//             tabPanel.hide(); //display:none;
+//             let target =$(this).attr('href')
+//             $(target).show();
+
+//          })
+//          tabAnchor.eq(0).trigger("click")
+//     });
 
 
 //게시판 swiper
 var swiper = new Swiper(".main03-noticeBox-swiper", {
     direction: 'vertical',
-  autoplay: {
-        delete:2000,
+    autoplay: {
+        delete: 2000,
         disableOnInteraction: false,
     },
-  loop: Infinity,
+    loop: Infinity,
 });
 
 //이벤트swiper
 var swiper2 = new Swiper(".main03-eventBox-swiper", {
     direction: 'vertical',
     autoplay: {
-        delete:2000,
+        delete: 2000,
         disableOnInteraction: false,
     },
-  loop: Infinity,
+    loop: Infinity,
 });
 
 //캠페인swiper
 var swiper3 = new Swiper(".main03-campaignBox-swiper", {
     direction: 'vertical',
-  autoplay: {
-        delete:2000,
+    autoplay: {
+        delete: 2000,
         disableOnInteraction: false,
     },
-  loop: Infinity,
+    loop: Infinity,
 });
 
 // 메인사이드메뉴
@@ -339,21 +349,13 @@ $(document).ready(function () {
 
 // 상단메뉴
 $('.header').hide();
-$(window).on('scroll',function(){
+$(window).on('scroll', function () {
 
-    if($(window).scrollTop() > 880){
+    if ($(window).scrollTop() > 880) {
         $('.header').fadeIn();
         $('.fixed-menu').fadeIn();
-    }else{
+    } else {
         $('.header').fadeOut();
         $('.fixed-menu').fadeOut();
     }
 })
-
-//rotatingslider
-$(function(){ 
-    $('.rotating-slider').rotatingSlider({
-		slideHeight : Math.min(300, window.innerWidth - 80),
-		slideWidth : Math.min(300, window.innerWidth - 80),
-	});
-});
